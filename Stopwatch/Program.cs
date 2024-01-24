1ddeef5b10b43875813760350f14d1242e9796b4
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-23 22:05:14 -0300

    CSharp

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Threading;
 
-namespace Stopwatch 
+namespace Stopwatch
 {
     class Program
     {
-        static void Main (string[] args)
+        static void Main(string[] args)
         {
-            
+            Menu();
+        }
+
+        static void Menu()
+        {
+
+            Console.Clear();
+            Console.WriteLine(" (S) = Segundos -> 10s -> 10 segundos \n (M) = Minutos -> 1m - 1 minuto \n (0) = Sair");
+            Console.WriteLine(" ");
+            Console.WriteLine("Quanto tempo deseja contar? ");
+
+            string data = Console.ReadLine().ToLower();
+            Console.WriteLine(data);
+
+
+        }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado... Retornando para o menu");
+            Thread.Sleep(2000);
         }
     }
 }
