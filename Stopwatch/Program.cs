52215df694b870f71da4b6f89418d846a36d04f0
Maicon Dutra <109883278+maicondutradev@users.noreply.github.com>
2024-01-23 22:31:32 -0300

    CSharp

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -19,11 +19,37 @@ namespace Stopwatch
             Console.WriteLine("Quanto tempo deseja contar? ");
 
             string data = Console.ReadLine().ToLower();
-            Console.WriteLine(data);
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
 
+            if (type == 'm')
+            {
+                multiplier = 60;
+            }
+
+            if (time == 0)
+            {
+                System.Environment.Exit(0);
+            }
+
+            PreStart(time * multiplier);
 
         }
 
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2000);
+
+            Start(time);
+
+        }
         static void Start(int time)
         {
             int currentTime = 0;
@@ -39,6 +65,8 @@ namespace Stopwatch
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado... Retornando para o menu");
             Thread.Sleep(2000);
+
+            Menu();
         }
     }
 }
--- Stopwatch/obj/Debug/net8.0/Stopwatch.AssemblyInfo.cs
+++ Stopwatch/obj/Debug/net8.0/Stopwatch.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b356ffcb8399564a153af9d2565b0fd20d86ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ddeef5b10b43875813760350f14d1242e9796b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Stopwatch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
